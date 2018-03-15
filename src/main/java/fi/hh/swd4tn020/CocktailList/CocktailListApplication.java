dc0664fa2ac5386e0cf07fa03befafbecc85585f
Tatu Erkinjuntti <tatu.erkinjuntti@gmail.com>
2018-03-15 22:14:10 +0200

    First commit

---
+++ src/main/java/fi/hh/swd4tn020/CocktailList/CocktailListApplication.java
@@ -0,0 +1,12 @@
+package fi.hh.swd4tn020.CocktailList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailListApplication.class, args);
+	}
+}
